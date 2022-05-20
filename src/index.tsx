f23678eeca208e2c47f9ddb465beda17982640c7
ncs2738 <ncs2738@rit.edu>
2022-05-20 03:52:58 -0400

    eslint & prettier added

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React, {FC} from "react";
+import React, { FC } from "react";
 import ReactDOM from "react-dom";
 import "./styles.scss";
 
-const App: FC = () =>
-{
-    return(
-        <div>
-            <h1>Howdy doo - this is the start of me resume!</h1>
-        </div>
-    )
-}
+const App: FC = () => {
+  return (
+    <div>
+      <h1>Howdy doo - this is the start of me resume!</h1>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
