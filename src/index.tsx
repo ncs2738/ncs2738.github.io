6c96160b8b18e4f15b6be27738a7f3b8f727c2eb
ncs2738 <ncs2738@rit.edu>
2022-05-20 02:52:15 -0400

    Base Setup

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from "react";
+import ReactDOM from "react-dom";
+import "./styles.scss";
+
+const App: FC = () =>
+{
+    return(
+        <div>
+            <h1>Howdy doo - this is the start of me resume!</h1>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
